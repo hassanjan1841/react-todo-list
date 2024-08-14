f556e8e670ee8dd6aeb5dc90bbb1e0e4196993cc
Hassan Jan <161922826+hassanjan1841@users.noreply.github.com>
2024-08-14 17:00:54 +0500

    almost done but the delete and completed is not set yer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import TodoList from "./components/TodoList";
+import Modal from "./components/Modal";
 
 function App() {
+  const [isTaskEmpty, setIsTaskEmpty] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Learn React",
+      category: "Programming",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: "Learn Vue",
+      category: "Personal",
+      isCompleted: false,
+    },
+  ]);
+
+  const handleTask = (task) => {
+    console.log(task);
+    if (task.text === "") {
+      setIsTaskEmpty(true);
+      return;
+    }
+    setTasks((prev) => [...prev, { ...task, id: prev.length + 1 }]);
+  };
+
   return (
     <>
-      <TodoList />
+      {isTaskEmpty && <Modal text="Task cannot be empty" />}
+      <TodoList tasks={tasks} handleTask={handleTask} />
     </>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import Button from "./Button";
 import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = ({ tasks, handleTask }) => {
+  const [task, setText] = useState({
+    text: "",
+    category: "Programming",
+    isCompleted: false,
+  });
+
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font ">
       <div className="container px-5 py-24 mx-auto">
-        <div className="border p-2 flex items-center justify-between text-center w-full mb-20">
+        <div className="border p-2 flex items-center justify-between text-center w-full mb-20 gap-4">
           <h1 className="flex-1 text-start sm:text-3xl text-2xl text-gray-900">
             Your Task
           </h1>
@@ -12,10 +20,29 @@ const TodoList = () => {
             type="text"
             placeholder="Type Your Task "
             className="border px-2 py-2 rounded-md flex-1"
+            onChange={(e) =>
+              setText((prev) => ({ ...prev, text: e.target.value }))
+            }
           />
+          <select
+            name=""
+            value={task.category}
+            id=""
+            className="px-2 py-2 rounded-md"
+            onChange={(e) =>
+              setText((prev) => ({ ...prev, category: e.target.value }))
+            }
+          >
+            <option value="Programming">Programming</option>
+            <option value="Personal">Personal</option>
+            <option value="Important">Important</option>
+          </select>
+          <Button text={"Add Task"} addTask={() => handleTask(task)} />
         </div>
         <div className="flex flex-wrap -m-2">
-          <Todo />
+          {tasks.map((task) => (
+            <Todo task={task} key={task.id} />
+          ))}
         </div>
       </div>
     </section>
